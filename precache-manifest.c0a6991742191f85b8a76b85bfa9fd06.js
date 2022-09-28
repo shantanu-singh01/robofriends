fed50d7560fac732d93556cb8bb692cf95e829a4
nanes00 <singhshantanu@gmail.com>
2022-09-28 10:41:20 +0530

    Updates

---
+++ precache-manifest.c0a6991742191f85b8a76b85bfa9fd06.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "409093657ac96bc55b56",
+    "url": "/robofriends/static/css/main.008a862f.chunk.css"
+  },
+  {
+    "revision": "409093657ac96bc55b56",
+    "url": "/robofriends/static/js/main.40909365.chunk.js"
+  },
+  {
+    "revision": "7ab2393a9ad8c2b58dcd",
+    "url": "/robofriends/static/js/1.7ab2393a.chunk.js"
+  },
+  {
+    "revision": "329b9987da521617bb41",
+    "url": "/robofriends/static/css/2.15d448e6.chunk.css"
+  },
+  {
+    "revision": "329b9987da521617bb41",
+    "url": "/robofriends/static/js/2.329b9987.chunk.js"
+  },
+  {
+    "revision": "cf2212719657acf7eaba",
+    "url": "/robofriends/static/js/runtime~main.cf221271.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "fc1359f0943f7a7f45eaf55594de05bf",
+    "url": "/robofriends/index.html"
+  }
+];
